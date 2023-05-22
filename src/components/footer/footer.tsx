45d79579e331f4380d00997a82c0955d5e9cdab1
DimitarDimitrov85 <dimitarivelinov85@gmail.com>
2023-05-22 19:29:45 +0300

    Created response of partners links.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -47,10 +47,10 @@ export const Footer = () => {
                 </div>
             </div>
             <h4 className='text-center' style={{color: 'white'}}>Нaшите партньори</h4>
-            <div className='text-center d-flex justify-content-between contacts-info'>
-                <a href='https://www.lights-photography.com' rel="noreferrer" target='_blank' title='www.lights-photography.com'><img src='/images/light.png' alt='lights-photography' width={100}/></a>
-                <a href='https://www.pavaresine.bg' rel="noreferrer" target='_blank' title='www.pavaresine.bg'><img src='/images/PavaResine.png' alt='pavaresine' width={100}/></a>
-                <a href='https://www.astudio.bg' rel="noreferrer" target='_blank' title='www.astudio.bg'><img src='/images/AStudio.jpg' alt='astudio' width={100}/></a>
+            <div className='d-flex justify-content-between partners'>
+                <a href='https://www.lights-photography.com' rel="noreferrer" target='_blank' title='www.lights-photography.com'><img src='/images/light.png' alt='lights-photography'/></a>
+                <a href='https://www.pavaresine.bg' rel="noreferrer" target='_blank' title='www.pavaresine.bg'><img src='/images/PavaResine.png' alt='pavaresine'/></a>
+                <a href='https://www.astudio.bg' rel="noreferrer" target='_blank' title='www.astudio.bg'><img src='/images/AStudio.jpg' alt='astudio'/></a>
             </div>
 
             <p className='text-center text-white copyright'>
